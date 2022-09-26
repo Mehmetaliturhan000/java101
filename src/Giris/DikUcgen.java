73df29017d98527ef7694b3abb93c251f4e2bae8
mehmetaliturhan000 <memeydos2000@gmail.com>
2022-09-27 01:25:38 +1000

    Üçgen alan ve hipotenüs hesaplama uygulaması yapıldı.

---
+++ src/Giris/DikUcgen.java
@@ -0,0 +1,40 @@
+package Giris;
+import java.util.Scanner;
+
+public class DikUcgen {
+    public static void main(String[] args) {
+        //Değişkenlerimizi tanımlayalım.
+
+        int a,b;
+        double alan,c;
+
+        //Kullanıcıdan verileri alalım
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("1.Kenarı Giriniz : ");
+        a = girdi.nextInt();
+        System.out.print("2.Kenarı Giriniz : ");
+        b = girdi.nextInt();
+
+        c = Math.sqrt((a *a ) + (b * b));
+        System.out.println("Hipotenüs : " + c);
+        alan = (a * b) / 2.0 ;
+        System.out.println("Dik Üçgenin Alanı : " + alan);
+
+
+        //Üç kenarının kullanıcıdan alınarak alanın hesaplayalım.
+        System.out.println("İkinci kısım : ");
+        double a2,b2,c2,alan2;
+        double u;
+        System.out.println("1.Kenarı Giriniz : ");
+        a2 = girdi.nextInt();
+        System.out.println("2.Kenarı Giriniz : ");
+        b2 = girdi.nextInt();
+        System.out.println("3.Kenarı Giriniz : ");
+        c2 = girdi.nextInt();
+        u = (a + b + c) / 2;
+        alan2 = Math.sqrt(u * (u - a2) * (u - b2) * (u - c2));
+        System.out.println("Üçgenin Alanı : " + alan2);
+
+
+    }
+}
--- src/Giris/KdvCalculator.java
+++ src/Giris/KdvCalculator.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class KdvCalculator {
     public static void main(String[] args) {
-        double tutar, kdvTutar, kdvliTutar, kdvOran = 0.18;
+        double tutar, kdvTutar, kdvliTutar,kdvOran;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Ücret tutarını giriniz.");
         tutar = input.nextDouble();
+
+        if(tutar <= 1000)
+            kdvOran = 0.18;
+
+        else
+            kdvOran = 0.08;
+
         kdvTutar = tutar * kdvOran;
         kdvliTutar = tutar + kdvTutar;
 

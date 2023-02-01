4a9fe18a68d888b17cbfe46108152f7a7c0b8c57
mehmetaliturhan000 <memeydos2000@gmail.com>
2023-02-01 22:33:31 +1100

    BirdenYuzeKadarAsal.java

---
+++ src/Giris/BirdenYuzeKadarAsal.java
@@ -0,0 +1,29 @@
+package Giris;
+
+public class BirdenYuzeKadarAsal {
+    public static void main(String[] args) {
+        boolean asalMi = true;
+
+        for(int i = 1; i <= 100;i++)
+        {
+            for(int j = 2; j < i; j++ )
+            {
+                if(i % j == 0)
+                {
+                    asalMi = false;
+                    break;
+                }
+                else {
+                    asalMi = true;
+                }
+            }
+
+            if(asalMi && i != 1)
+            {
+                System.out.print(i + " ");
+            }
+
+        }
+
+    }
+}
